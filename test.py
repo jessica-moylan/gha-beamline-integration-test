44e2f993986b1202ba6d14b6aafca0f938c5b8a1
jessica-moylan <jessicamoylan10@gmail.com>
2026-06-25 11:10:33 -0400

    corrected capitalization for input

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-a = input("What is your name?")
-print(f"Hello, {a}!")
+from enum import Enum
+
+class ENDSTATION_ENUM(Enum):
+    SIX = "six"
+    Keithley = "keithley"
+
+
+print("Please select an endstation from:")
+for e in ENDSTATION_ENUM:
+    print(f"\t- {e.name}")
+endstation_choice = input("Enter your selection: ")
+try:
+    endstation = ENDSTATION_ENUM[endstation_choice]
+except KeyError as e:
+    raise Exception(
+        f"Endstation choice '{endstation_choice}' is not one of the valid options."
+    ) from e
+print(f"You selected: {endstation.name} ({endstation.value})")
